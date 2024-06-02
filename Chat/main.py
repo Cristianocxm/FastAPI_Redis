b5c04370acb06f1b4a2fa0986247e92fa1093398
Cristianocxm <114105719+Cristianocxm@users.noreply.github.com>
2024-06-02 10:58:01 -0300

    Add files via upload

---
+++ Chat/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import redis
+from datetime import datetime
+import json
+
+
+app = FastAPI()
+
+# Monta o diretório 'static' para servir arquivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Configura o diretório de templates
+templates = Jinja2Templates(directory="templates")
+
+rd = redis.Redis(host="localhost", port=6379, db=0)
+print(rd)
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.get("/", response_class=HTMLResponse)
+async def get(request: Request):
+    context = {'request': request}
+    return templates.TemplateResponse("index.html", context)
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Client #{client_id} diz: {data}")
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            message = [data, timestamp]
+            print(f'chat:{client_id}', message)
+            print(message)
+            rd.set(f'chat:{client_id}', json.dumps(message))
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
